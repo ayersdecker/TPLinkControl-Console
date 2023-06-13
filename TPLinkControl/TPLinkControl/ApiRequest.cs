4569da60761b3add2b0a3e7c55836947e42a70ce
ayersdecker <ayersdecker@gmail.com>
2023-06-13 18:47:51 -0400

    Work End, Pick Up Tomorrow

---
+++ TPLinkControl/TPLinkControl/ApiRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TPLinkControl
+{
+    public class ApiRequest
+    {
+        public async Task<dynamic> ResponseDynamic(string url, string un, string pw)
+        {
+            var body = 
+            // Builds Request
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion");
+            client.DefaultRequestHeaders.Add()
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            // Get JSON Data
+            string content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<dynamic>(content);
+        }
+    }
+}
---
+++ TPLinkControl/TPLinkControl/Program.cs
@@ -0,0 +1,85 @@
+﻿
+
+using TPLinkControl;
+
+ApiRequest apiRequest = new ApiRequest();
+
+while (true)
+{
+
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            Console.WriteLine("\nDatabase created.");
+            break;
+        case "2":
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            Console.WriteLine("\nTables created.");
+            break;
+        case "3":
+            Console.ForegroundColor = ConsoleColor.Green;
+           
+            Console.WriteLine("\nData inserted.");
+            break;
+        case "4":
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            
+            Console.WriteLine("\nData updated.");
+            break;
+        case "5":
+            Console.ForegroundColor = ConsoleColor.Red;
+            
+            Console.WriteLine("\nData deleted.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            break;
+        case "6":
+            Console.ForegroundColor = ConsoleColor.Red;
+            
+            Console.WriteLine("\nDatabase deleted.");
+            break;
+        case "7":
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            
+            break;
+        case "8":
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            
+            break;
+
+        case "9":
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nExiting program.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            return;
+        default:
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nInvalid option.");
+            break;
+
+    }
+    
+}
+void Login()
+{
+    bool flag = false;
+    while (flag)
+    {
+        Console.WriteLine("TPLink Control Console\n");
+        Console.Write("Username: ");
+        string un = Console.ReadLine();
+        Console.Write("\nPassword: ");
+        string pw = Console.ReadLine();
+
+        apiRequest.ResponseDynamic();
+
+
+    }
+}
+void Menu()
+{
+
+}
