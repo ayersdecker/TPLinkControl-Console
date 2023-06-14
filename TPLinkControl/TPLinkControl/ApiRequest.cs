98c7a38897529dea4d68ecd3c9c1856f526094eb
ayersdecker <ayersdecker@gmail.com>
2023-06-14 14:51:41 -0400

    Finished Basic Light Controls

--- TPLinkControl/TPLinkControl/ApiRequest.cs
+++ TPLinkControl/TPLinkControl/ApiRequest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -9,14 +10,66 @@ namespace TPLinkControl
 {
     public class ApiRequest
     {
-        public async Task<dynamic> ResponseDynamic(string url, string un, string pw)
+        public async Task<dynamic> LoginDynamic(string un, string pw)
         {
-            var body = 
+            
+
+
+            var json = "{\n \"method\": \"login\",\n \"params\": {\n \"appType\": \"Kasa\",\n \"cloudUserName\": \"" + $"{un}" + "\",\n \"cloudPassword\": \"" + $"{pw}" + "\",\n \"terminalUUID\": \"4c840107-5353-4a7a-b46e-77dd1d72f526\"}"+"}";
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://wap.tplinkcloud.com"),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
             // Builds Request
             HttpClient client = new HttpClient();
             client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion");
-            client.DefaultRequestHeaders.Add()
-            HttpResponseMessage response = await client.GetAsync(url);
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            // Get JSON Data
+            string content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<dynamic>(content);
+        }
+        public async Task<dynamic> ShowDevicesDynamic(string token)
+        {
+            var json = "{\n \"method\": \"getDeviceList\""+ "}";
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://wap.tplinkcloud.com?token=" + $"{token}"),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            // Builds Request
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion");
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            // Get JSON Data
+            string content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<dynamic>(content);
+        }
+        public async Task<dynamic> TurnDevice(string token, string state, string deviceID, string color, string light)
+        {
+            var json = "{\n \"method\":\"passthrough\", \n \"params\": {\n \"deviceId\": \"" + $"{deviceID}" + "\", \n \"requestData\": {\"smartlife.iot.smartbulb.lightingservice\":{\"transition_light_state\":{\"on_off\":" + $"{state}" + ", \"brightness\":" + $"{light}" + ",\"hue\":" + $"{color}" + ",\"saturation\":100}" + "}} }}";
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://wap.tplinkcloud.com?token=" + $"{token}"),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            // Builds Request
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion");
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var response = await client.SendAsync(request);
             response.EnsureSuccessStatusCode();
 
             // Get JSON Data
@@ -24,4 +77,5 @@ namespace TPLinkControl
             return JsonConvert.DeserializeObject<dynamic>(content);
         }
     }
+    
 }
--- TPLinkControl/TPLinkControl/Program.cs
+++ TPLinkControl/TPLinkControl/Program.cs
@@ -1,55 +1,57 @@
-﻿
+﻿using TPLinkControl;
 
-using TPLinkControl;
-
+// Loading Static Variables
 ApiRequest apiRequest = new ApiRequest();
+dynamic login = "";
+bool firstEscape = false;
 
+// Loop to capture failed Logins
+while(!firstEscape) 
+{ 
+    firstEscape = await Login();
+    if (!firstEscape)
+    {
+        Console.Clear();
+        Console.WriteLine("Login Failed - - Try Again \n");
+    }
+}
+
+// Quick Confirm
+Console.WriteLine("Successful Login");
+
+// Main Program Loop
 while (true)
 {
-
-
+    Console.ForegroundColor = ConsoleColor.Green;   
+    // Calls Menu Method with Nickname from Login Dynamic
+    Menu(login.result.nickname);
+    // Route Panel
     switch (Console.ReadLine())
     {
         case "1":
             Console.ForegroundColor = ConsoleColor.Green;
-            
-            Console.WriteLine("\nDatabase created.");
+            await ShowDevices(login.result.token);
             break;
         case "2":
             Console.ForegroundColor = ConsoleColor.Green;
-            
-            Console.WriteLine("\nTables created.");
+            await TurnOnLight(login.result.token);
             break;
         case "3":
             Console.ForegroundColor = ConsoleColor.Green;
-           
-            Console.WriteLine("\nData inserted.");
+            await TurnOffLight(login.result.token);
             break;
         case "4":
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            
-            Console.WriteLine("\nData updated.");
+            Console.ForegroundColor = ConsoleColor.Green;
+            await TurnRedLight(login.result.token);
             break;
         case "5":
-            Console.ForegroundColor = ConsoleColor.Red;
-            
-            Console.WriteLine("\nData deleted.");
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Green;
+            await TurnGreenLight(login.result.token);
             break;
         case "6":
-            Console.ForegroundColor = ConsoleColor.Red;
-            
-            Console.WriteLine("\nDatabase deleted.");
+            Console.ForegroundColor = ConsoleColor.Green;
+            await TurnBlueLight(login.result.token);
             break;
-        case "7":
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            
-            break;
-        case "8":
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            
-            break;
-
         case "9":
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nExiting program.");
@@ -59,27 +61,100 @@ while (true)
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nInvalid option.");
             break;
-
     }
-    
 }
-void Login()
+// Login Sequence That calls Api Request with given Login Creds
+async Task<bool> Login()
 {
-    bool flag = false;
-    while (flag)
-    {
         Console.WriteLine("TPLink Control Console\n");
         Console.Write("Username: ");
         string un = Console.ReadLine();
         Console.Write("\nPassword: ");
         string pw = Console.ReadLine();
-
-        apiRequest.ResponseDynamic();
-
-
-    }
+    // Request Call
+        login = await apiRequest.LoginDynamic(un, pw);
+    // Return Type
+        if (login.error_code != 0)
+        {
+            return false;
+        }
+        else { return true; }
 }
-void Menu()
-{
 
+// Basic Control Menu
+void Menu(string nn)
+{
+    Console.Clear();
+    Console.WriteLine("TPLink Control Console");
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine($"\nWelcome {nn}!");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("\n\n1. Show Devices");
+    Console.WriteLine("2. Turn On Light");
+    Console.WriteLine("3. Turn Off light");
+    Console.WriteLine("4. Turn On and Red");
+    Console.WriteLine("5. Turn On and Blue");
+    Console.WriteLine("6. Turn On and Green");
+    Console.WriteLine("9. Exit");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("\nSelection: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+}
+// ShowDevices Sequence That calls Api Request with provided Token
+async Task ShowDevices(string token)
+{
+    // Initial Color
+    Console.ForegroundColor= ConsoleColor.Yellow;
+    Console.WriteLine();
+
+    // Request
+    var body = await apiRequest.ShowDevicesDynamic(token);
+    var devices = body.result.deviceList;
+    string id = body.result.deviceList[0].deviceId;
+    for(int i = 0; i < devices.Count; i++) 
+    { 
+        Console.WriteLine($"Device {i+1}: " +devices[i].deviceId); 
+    }
+    Console.WriteLine("\n\n[ Press ENTER to Continue ]");
+    Console.ReadLine();
+}
+// Light (OFF) Sequence That calls Api Request with provided Token
+async Task TurnOffLight(string token)
+{
+    // Request on First Device Only.
+    var body = await apiRequest.ShowDevicesDynamic(token);
+    string id = body.result.deviceList[0].deviceId;
+    var action = await apiRequest.TurnDevice(token, "0", id, "50", "100");
+}
+// Light (ON) Sequence That calls Api Request with provided Token
+async Task TurnOnLight(string token)
+{
+    // Request on First Device Only.
+    var body = await apiRequest.ShowDevicesDynamic(token);
+    string id = body.result.deviceList[0].deviceId;
+    var action = await apiRequest.TurnDevice(token, "1", id, "50", "100");
+}
+// Light (RED) Sequence That calls Api Request with provided Token
+async Task TurnRedLight(string token)
+{
+    // Request on First Device Only.
+    var body = await apiRequest.ShowDevicesDynamic(token);
+    string id = body.result.deviceList[0].deviceId;
+    var action = await apiRequest.TurnDevice(token, "1", id, "0", "50");
+}
+// Light (GREEN) Sequence That calls Api Request with provided Token
+async Task TurnGreenLight(string token)
+{
+    // Request on First Device Only.
+    var body = await apiRequest.ShowDevicesDynamic(token);
+    string id = body.result.deviceList[0].deviceId;
+    var action = await apiRequest.TurnDevice(token, "1", id, "100", "50");
+}
+// Light (BLUE) Sequence That calls Api Request with provided Token
+async Task TurnBlueLight(string token)
+{
+    // Request on First Device Only.
+    var body = await apiRequest.ShowDevicesDynamic(token);
+    string id = body.result.deviceList[0].deviceId;
+    var action = await apiRequest.TurnDevice(token, "1", id, "240", "50");
 }
